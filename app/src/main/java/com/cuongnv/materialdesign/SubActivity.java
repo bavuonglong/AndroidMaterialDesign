5b3724a0bed4b9a83a0f933998fe9a0fd47924f9
CuongNV <cuong.ngo@axonactive.vn>
2016-12-24 23:35:28 +0700

    - add more action on toolbar - click an action on toolbar to navigate to another activity - add and handle: back to previous activity action https://www.youtube.com/watch?v=EAZv1fP-5TM

--- app/src/main/java/com/cuongnv/materialdesign/SubActivity.java
+++ app/src/main/java/com/cuongnv/materialdesign/SubActivity.java
@@ -1,28 +1,32 @@
 package com.cuongnv.materialdesign;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class SubActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sub);
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_sub, menu);
         return true;
     }
 
@@ -31,12 +35,12 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.action_setting) {
-            Toast.makeText(this, "Hit Setting", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Hit Setting in sub activity", Toast.LENGTH_SHORT).show();
             return true;
         }
 
-        if (id == R.id.navigate) {
-            startActivity(new Intent(this, SubActivity.class));
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
         }
         return super.onOptionsItemSelected(item);
     }
