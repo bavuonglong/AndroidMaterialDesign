01db2a4fbc0a4f0b4bd8b32e1e435467072bd083
CuongNV <cuong.ngo@axonactive.vn>
2016-12-25 20:32:16 +0700

    - add navigation drawer https://www.youtube.com/watch?v=zWpEh9k8i7Q https://www.youtube.com/watch?v=tR2K_Sav7q8 https://www.youtube.com/watch?v=AsbtXnJ1zww https://www.youtube.com/watch?v=TBKpsGar5mc

--- app/src/main/java/com/cuongnv/materialdesign/MainActivity.java
+++ app/src/main/java/com/cuongnv/materialdesign/MainActivity.java
@@ -2,6 +2,7 @@ package com.cuongnv.materialdesign;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -18,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
     }
 
     @Override
